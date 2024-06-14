6bfcb5bf3642e4a254da9c842f0dff3efd6aa20d
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-06-14 07:17:05 +0100

    Update chains.ts

--- src/consts/chains.ts
+++ src/consts/chains.ts
@@ -4,10 +4,14 @@ import { defineChain } from "thirdweb";
 export const bitrock = defineChain({
   chainId: 7171,
   name: "Bitrock",
-  
+  rpc: ["https://connect.bit-rock.io"], // Replace with actual RPC URL
   nativeCurrency: {
     name: "Bitrock",
     symbol: "BROCK",
     decimals: 18,
   },
+  blockExplorers: [{ name: "Bitrock Explorer", url: "https://explorer.bit-rock.io" }], // Replace with actual explorer URL
+  testnet: false,
+  slug: "bitrock",
+  shortName: "brock"
 });
