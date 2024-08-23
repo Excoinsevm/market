59a8f65f4f71ce6a4a5b1fc1d9c4d336d3ea3708
Excoinsevm <149539033+Excoinsevm@users.noreply.github.com>
2024-08-23 17:34:35 +0100

    Update nft_contracts.ts

--- src/consts/nft_contracts.ts
+++ src/consts/nft_contracts.ts
@@ -20,6 +20,14 @@ export type NftContract = {
  */
 export const NFT_CONTRACTS: NftContract[] = [
   {
+      address: "0x281F8a70b8DE841266461C36CD75B8E68B9647E0",
+    chain: bitrock,
+    title: "Lucky Charm Time & Space",
+    thumbnailUrl:
+      "https://1359df3adf00c991ec1e8d3ac7e1d8ad.ipfscdn.io/ipfs/QmbcJHZooVP22BM9ZTRxLWNNnPzdtQxF9AhTGXoJyZxpTF/ezgif-2-e69b48ff61.gif",
+    type: "ERC721",
+  },
+  {
     address: "0x8dDEC53Ad0FbBd07FC1e5E5C238dC8B9DcE45557",
     chain: bitrock,
     title: "Brock Computer Cats",
@@ -34,13 +42,5 @@ export const NFT_CONTRACTS: NftContract[] = [
     thumbnailUrl:
       "https://1359df3adf00c991ec1e8d3ac7e1d8ad.ipfscdn.io/ipfs/QmYSfsK91RBifXmTxkAq6qw1ddGYZBFkbxXm1CojTWF4Yt/Untitled%20design.gif",
     type: "ERC1155",
-  },
-  {
-      address: "0x281F8a70b8DE841266461C36CD75B8E68B9647E0",
-    chain: bitrock,
-    title: "Lucky Charm Time & Space",
-    thumbnailUrl:
-      "https://1359df3adf00c991ec1e8d3ac7e1d8ad.ipfscdn.io/ipfs/QmbcJHZooVP22BM9ZTRxLWNNnPzdtQxF9AhTGXoJyZxpTF/ezgif-2-e69b48ff61.gif",
-    type: "ERC721",
   },
 ];
